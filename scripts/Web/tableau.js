29f080aecae54c339e03ccefbf7f30044fcf5aac
Amy <kimam@utschools.ca>
2019-02-20 12:44:21 -0500

    added accordions and additional sliders

--- scripts/Web/tableau.js
+++ scripts/Web/tableau.js
@@ -1,5 +1,6 @@
-var params_short = ["Population", "Education", "Lifestyle"];
-var params_long = []
+var params0 = ["Population", "Education", "Lifestyle"];
+var params_ed = ["College","Grad","Ugrad","Patents"];
+var params_li = ["Commute","Crime","Housing"];
 
 //Loading tableau visualization on initial page load
 function initViz() {
@@ -20,7 +21,7 @@ function submit_param(param,val){
 }
 
 //Helper function to find the sum of all parameter values
-function get_sum_params(){
+function get_sum_params(params){
   var sum = 0;
   for (j = 0; j < params.length; j++){
     sum += parseFloat(document.getElementById(params[j]).value);
@@ -31,12 +32,15 @@ function get_sum_params(){
 
 //Function to change a parameter value (using sliders)
 function change_val(param,opt=0) {
-  params = params_short
-  //Full parameter options?
-  if (opt){
-    params = params_long
+  var params;
+  if (opt==1) {
+    params = params_ed
+  }else if (opt==2) {
+    params = params_li
+  }else {
+    params = params0
   }
-  var sum = get_sum_params();
+  var sum = get_sum_params(params);
 
   //Displaying normalized values
   for (i = 0; i < params.length; i++){
@@ -49,4 +53,24 @@ function change_val(param,opt=0) {
     var slider = document.getElementById(param);
     submit_param(param,slider.value/100)
   }
+}
+
+var acc = document.getElementsByClassName("accordion");
+var k;
+
+for (k = 0; k < acc.length; k++) {
+  acc[k].addEventListener("click", function() {
+    console.log('hi')
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
