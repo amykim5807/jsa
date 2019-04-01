8795c16a48ff5bc5d809da833447f079cba9030e
Amy <kimam@utschools.ca>
2019-04-01 14:53:23 -0400

    edits

--- scripts/R/clean_cities_all.R
+++ scripts/R/clean_cities_all.R
@@ -4,7 +4,6 @@ library(ggmap)
 #Importing Raw Data
 data <- read.csv("data/areacompare.csv")
 source('scripts/R/helper.R')
-apikey <- "AhOEjpt6lpUWgRVwwNI7WfkXVbAt97SgCkNlc8dS-IgciqusrkvVWRUo3Z23mMe1"
 
 #Renaming Columns, deleting first row and irrelevant columns
 data <- data[-c(1),-c(1,2,4,19) ]
@@ -13,15 +12,7 @@ colnames(data) <- c('MSA','Total Population', indices, 'Kauffman','Site selectio
 
 #Retrieving and attaching longitude and latitude data for all MSAs
 new_loc <- unlist(lapply(as.character(data$MSA)[1:nrow(data)],correct_loc))
-loc <- geocode(new_loc[1:10],source='dsk')
-
-  
-
-devtools::install_github("gsk3/taRifx.geo")
-options(BingMapsKey=apikey)
-#geocodeVect <- Vectorize(geocode, vectorize.args="location")
-loc2 <- geocode("Abilene, TX", service="bing", returntype="coordinates")
-
+loc <- geocode(new_loc,source='dsk')
 data$Longitude <- loc$lon
 data$Latitude <- loc$lat
 # 
